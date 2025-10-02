84784aa9beaf6f82ba62cd68fb3239fec36ba0a4
GuilhermeSerafim <guilerstudies@gmail.com>
2025-10-02 17:42:40 -0300

    feat: base cadastro

--- src/app/pages/cadastro/cadastro.ts
+++ src/app/pages/cadastro/cadastro.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { Footer } from '../../components/footer/footer';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [Header, Footer, FormsModule, CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    Header,
+    Footer,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
-export class Cadastro {}
+export class Cadastro {
+temConta() {
+throw new Error('Method not implemented.');
+}
+hidePassword: boolean = true;
+  onSubmit(_t8: NgForm) {
+    throw new Error('Method not implemented.');
+  }
+}
