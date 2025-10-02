3d1aae94e2a05fec93f4b16c3212f22465b1fc17
GuilhermeSerafim <guilerstudies@gmail.com>
2025-10-02 15:49:15 -0300

    feat: page entre

--- src/app/pages/entre/entre.ts
+++ src/app/pages/entre/entre.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
-import { Header } from "../../components/header/header";
-import { Footer } from "../../components/footer/footer";
+import { Header } from '../../components/header/header';
+import { Footer } from '../../components/footer/footer';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-entre',
-  imports: [Header, Footer],
+  imports: [
+    CommonModule,
+    FormsModule,
+    Header,
+    Footer,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './entre.html',
-  styleUrl: './entre.scss'
+  styleUrl: './entre.scss',
 })
 export class Entre {
+  // Adicione a propriedade para controlar a visibilidade da senha
+  hidePassword = true;
 
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log('Formulário válido:', form.value);
+    } else {
+      console.log('Formulário inválido.');
+    }
+  }
+
+  aoClicarEmCriarUmaConta() {
+    // Sua lógica aqui
+    console.log('Clicou em Criar Conta');
+  }
+
+  aoEsquecerASenha() {
+    // Sua lógica aqui
+    console.log('Clicou em Esqueceu a Senha');
+  }
 }
