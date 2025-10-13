a67c71af150ecbf93a599282cba9e2d5411a4ff0
GuilhermeSerafim <guilerstudies@gmail.com>
2025-10-13 15:51:20 -0300

    feat(court): Implement availability check based on time and day of the week

--- src/app/components/select-court/select-court.ts
+++ src/app/components/select-court/select-court.ts
@@ -3,11 +3,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { ICourt } from '../../interfaces/icourt';
 import { CourtService } from '../../services/court.service';
 import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-select-court',
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './select-court.html',
   styleUrl: './select-court.scss',
 })
@@ -19,4 +19,31 @@ export class SelectCourt {
   ngOnInit(): void {
     this.courtService.getCourts().subscribe((courts) => (this.courts = courts));
   }
+
+isCourtAvailable(court: ICourt): boolean {
+  const now = new Date();
+
+  const diasDaSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+  const diaDaSemanaHoje = diasDaSemana[now.getDay()];
+  
+  if (!court.diasDisponiveis?.includes(diaDaSemanaHoje)) {
+    return false; // Se não estiver na lista de dias disponíveis, já retorna falso.
+  }
+
+  const horarioAberturaHoje = new Date(now); // Começa com a data de hoje
+  horarioAberturaHoje.setHours(
+    court.horarioAbertura!.getHours(),
+    court.horarioAbertura!.getMinutes(),
+    0, 0 // Zera segundos e milissegundos para precisão
+  );
+
+  const horarioFechamentoHoje = new Date(now); // Começa com a data de hoje
+  horarioFechamentoHoje.setHours(
+    court.horarioFechamento!.getHours(),
+    court.horarioFechamento!.getMinutes(),
+    0, 0 // Zera segundos e milissegundos
+  );
+
+  return now >= horarioAberturaHoje && now <= horarioFechamentoHoje;
+}
 }
