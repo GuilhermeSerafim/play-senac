9c77323e3194e186a210c82410247bbf1a0e7d8f
GuilhermeSerafim <guilerstudies@gmail.com>
2025-10-19 10:06:39 -0300

    feat: Add createTime helper function to generate Date objects with specified hours and minutes

---
+++ src/app/helpers/date.helpers.ts
@@ -0,0 +1,14 @@
+// Em: src/app/core/helpers/date.helpers.ts
+
+/**
+ * Cria um objeto Date com a data de hoje, mas com a hora e minuto especificados.
+ * Ideal para representar horários sem se preocupar com uma data específica.
+ * @param hour A hora (0-23)
+ * @param minute O minuto (0-59)
+ * @returns Um objeto Date.
+ */
+export const createTime = (hour: number, minute: number = 0): Date => {
+  const date = new Date();
+  date.setHours(hour, minute, 0, 0);
+  return date;
+};
