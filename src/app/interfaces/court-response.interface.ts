5cda2b92dd60e81049a2fab0fc24957ae2e263c6
GuilhermeSerafim <guilerstudies@gmail.com>
2025-11-21 23:31:51 -0300

    feat: refactor CourtService to use HTTP for CRUD operations and load courts from API

---
+++ src/app/interfaces/court-response.interface.ts
@@ -0,0 +1,8 @@
+export interface CourtResponse {
+  id: number;
+  nome: string;
+  imagemUrl: string;
+  limiteJogadores: number;
+  horarioAbertura: string | null;
+  horarioFechamento: string | null;
+}
--- src/app/services/court.service.ts
+++ src/app/services/court.service.ts
@@ -1,48 +1,102 @@
 import { Injectable } from '@angular/core';
 import { ICourt, ICreateCourt } from '../interfaces/icourt';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { mockListaDeQuadras } from '../mock/allmocks';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { ReservaService } from './reserva.service';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { CourtResponse } from '../interfaces/court-response.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CourtService {
-  private courtSubject = new BehaviorSubject<ICourt[]>(mockListaDeQuadras);
+
+  private readonly API_URL = `${environment.apiUrl}/quadras`;
+
+  private courtSubject = new BehaviorSubject<ICourt[]>([]);
   public court$: Observable<ICourt[]> = this.courtSubject.asObservable();
   
-  constructor(private readonly _reservaService: ReservaService){}
+  constructor(private readonly http: HttpClient){
+    this.loadCourts();
+  }
+
+  loadCourts() {
+    this.http.get<CourtResponse[]>(this.API_URL)
+    .pipe(
+      map((listaJava) => listaJava.map(item => this.adapter(item)))
+    )
+    .subscribe({
+      next: (listaPronta) => {
+        this.courtSubject.next(listaPronta);
+      },
+      error: (err) => {
+        console.error('Erro ao carregar quadras:', err);
+      }
+    })
+  }
 
   getCourts(): Observable<ICourt[]> {
     return this.court$;
   }
 
   removeCourt(idCourt: number): void {
-    // Removendo reservas
-    this._reservaService.removeReservesByCourtId(idCourt);
-    // Removendo quadra
-    const listaAtual = this.courtSubject.getValue();
-    const novaListaDeQuadras = listaAtual.filter((quadra) => quadra.id !== idCourt);
-    this.courtSubject.next(novaListaDeQuadras);
+    this.http.delete(`${this.API_URL}/${idCourt}`).subscribe({
+      next: () => {
+        this.loadCourts();
+      },
+      error: (err) => {
+        console.error('Erro ao remover quadra:', err);
+      }
+    });
   }
 
   addCourt(quadra: ICreateCourt): void {
-    const listaAtual = this.courtSubject.getValue();
-    const maxId = listaAtual.reduce((max, r) => (r.id > max ? r.id : max), 0);
-    const novaQuadraComId: ICourt = {
-      ...quadra,
-      id: maxId + 1,
+    const payloadJava = {
+      nome: quadra.title,
+      limiteJogadores: quadra.capacidade,
+      imagemUrl: quadra.pathImg,
+      horarioAbertura: quadra.horarioAbertura!.toISOString(),
+      horarioFechamento: quadra.horarioFechamento!.toISOString(),
     };
 
-    const novaListaDeQuadras: ICourt[] = [...listaAtual, novaQuadraComId];
-    this.courtSubject.next(novaListaDeQuadras);
+    this.http.post(this.API_URL, payloadJava).subscribe({
+      next: () => {
+        this.loadCourts();
+      },
+      error: (err) => {
+        console.error('Erro ao adicionar quadra:', err);
+      }
+    });
   }
 
   updateCourt(updatedCourt: ICourt): void {
-    const currentCourts = this.courtSubject.getValue();
-    const newCourtsList = currentCourts.map((court) =>
-      court.id === updatedCourt.id ? updatedCourt : court
-    );
-    this.courtSubject.next(newCourtsList);
+   const payloadJava = {
+      nome: updatedCourt.title,
+      limiteJogadores: updatedCourt.capacidade,
+      imagemUrl: updatedCourt.pathImg,
+      horarioAbertura: updatedCourt.horarioAbertura!.toISOString(),
+      horarioFechamento: updatedCourt.horarioFechamento!.toISOString(),
+    };
+    this.http.put(`${this.API_URL}/${updatedCourt.id}`, payloadJava).subscribe({
+      next: () => {
+        this.loadCourts();
+      },
+      error: (err) => {
+        console.error('Erro ao atualizar quadra:', err);
+      }
+    });
+  }
+
+  private adapter(backendData: CourtResponse): ICourt {
+    return {
+      id: backendData.id, 
+      title: backendData.nome,             
+      pathImg: backendData.imagemUrl,      
+      capacidade: backendData.limiteJogadores,
+      // Tratamento para datas (se vier null, cria data atual ou trata como preferir)
+      horarioAbertura: backendData.horarioAbertura ? new Date(backendData.horarioAbertura) : new Date(), 
+      horarioFechamento: backendData.horarioFechamento ? new Date(backendData.horarioFechamento) : new Date(),
+      diasDisponiveis: []
+    };
   }
 }
